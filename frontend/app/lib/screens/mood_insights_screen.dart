aeb6dc7fbca4b430ead844e058805bebc9d08a0b
Hazardo911 <aayushdhuri99999@gmail.com>
2026-02-27 18:11:46 +0530

    Reorganize frontend into frontend/app project layout

---
+++ frontend/app/lib/screens/mood_insights_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoodInsightsScreen extends StatelessWidget {
+  const MoodInsightsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Mood insights')));
+  }
+}
