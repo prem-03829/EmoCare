7b4c5313e8281a5639762944ca58dc316e9d54f6
Hazardo911 <aayushdhuri99999@gmail.com>
2026-02-27 20:31:57 +0530

    Track supabase config file

---
+++ frontend/app/lib/config/supabase_config.dart
@@ -0,0 +1,5 @@
+class SupabaseConfig {
+  static const String url = "https://qyyxwefzytlfrtucykbc.supabase.co";
+  static const String anonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5eXh3ZWZ6eXRsZnJ0dWN5a2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NjA1OTMsImV4cCI6MjA4NzQzNjU5M30.VulLKxYAQIZYaYA8B4tP58gBR9B1qH6rUjeVaK6SZ8A";
+}
